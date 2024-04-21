c864aa555ae018b0a4092d21021ae9fcbd64e949
Amit <amitkm.4211@gmail.com>
2024-04-21 23:04:17 +0530

    Test3

--- client/src/Avatar.jsx
+++ client/src/Avatar.jsx
@@ -18,7 +18,7 @@ const Avatar = ({ userId, username, online }) => {
 		<div
 			className={`w-12 h-12 relative ${color} text-white flex items-center justify-center rounded-full`}
 		>
-			<h1>{username[0]}</h1>
+			<h1>{username}</h1>
 			<div
 				className={`absolute bottom-0 right-0 w-[0.8rem] h-[0.8rem] rounded-full  border border-white ${
 					online ? 'bg-green-600' : 'bg-gray-400'
